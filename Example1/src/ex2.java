16cede31bb1cee4e4ffa8fade1bedb1ebf093f3e
mekey <mekey@DESKTOP-GSFCIQC>
2022-07-16 01:52:25 +0900

    33

---
+++ Example1/src/ex2.java
@@ -0,0 +1,17 @@
+
+public class ex2 {
+
+	public static void main(String[] args) {
+		String a = "ȫ�浿";
+		abc ab = new abc();
+		ab.name(a);
+	}
+
+}
+
+class abc{
+		public void name(String aa) {
+			System.out.println(aa);
+		}
+
+}
