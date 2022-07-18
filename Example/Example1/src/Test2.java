d051acb3c22c183317b91b4f82d1ae608a82ba6f
tjoeun <tjoeun@DESKTOP-LESBHHQ>
2022-07-18 10:58:29 +0900

    11233

---
+++ Example/Example1/src/Test2.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String a = "honggidong";
+		String b[] = a.split("");
+		System.out.println(Arrays.toString(b));
+
+	}
+
+}
